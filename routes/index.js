ea7784e17ab0f8610b76abfb61b5e5b05ec18bb8
Anca Stoica <ancastoica93@gmail.com>
2017-12-08 11:19:48 +0100

    created user profile

--- routes/index.js
+++ routes/index.js
@@ -37,15 +37,16 @@ MongoClient.connect("mongodb://localhost/bdd_planiz", function(err, db) {
     router.post('/createPlaniz', urlEncodedParser, function (req, res) {
         planiz_object = req.body;
         var id_user = uuidv1(); //random id user
+        var user = {id: id_user, name: planiz_object.username};
 
         var newObj = {
             title: planiz_object.title,
             description: planiz_object.description,
             destinations: [],
-            users: [{id: id_user, name: planiz_object.username}]
+            users: [user]
         };
 
-        //req.session.userId = id_user;
+        req.session.userId = id_user;
 
         db.collection("planiz").insert(newObj, null, function (err, result) {
             if (err) {
@@ -53,7 +54,7 @@ MongoClient.connect("mongodb://localhost/bdd_planiz", function(err, db) {
             } else {
 
                 var objectId = newObj._id;
-                res.redirect('/'+objectId+'/dashboard');
+                res.redirect('/'+objectId+'/'+id_user+'/dashboard');
             }
         });
     });
@@ -65,62 +66,154 @@ MongoClient.connect("mongodb://localhost/bdd_planiz", function(err, db) {
             if (err) {
                 console.error('Find failed', err);
             } else {
-                res.render('dashboard', {title:result.title, planiz: result})
+                if(req.session.userId){
+                    res.redirect('/'+req.params.planiz_id+'/'+req.session.userId+'/dashboard')
+                }
+                else{
+                    res.redirect('/'+req.params.planiz_id+'/login')
+                }
+
             }
         });
     });
 
     /* GET homepage/dashboard for planiz_id */
-    router.get('/:planiz_id/dashboard', function (req, res) {
+    router.get('/:planiz_id/:user_id/dashboard', function (req, res) {
         var o_id = new mongo.ObjectID(req.params.planiz_id);
         db.collection("planiz").findOne({"_id": o_id}, function (err, result) {
             if (err) {
                 console.error('Find failed', err);
             } else {
-                res.render('dashboard', {title:result.title, planiz: result})
+                if(req.session.userId) {
+                    var users = result.users;
+                    for (var i = 0; i < users.length; i++) {
+                        // look for the entry with a matching `user_id` value
+                        if (users[i].id == req.params.user_id) {
+                            var user = users[i];
+                        }
+                    }
+                    res.render('dashboard', {title: result.title, planiz: result, user:user})
+                }
+                else{
+                    res.redirect('/'+req.params.planiz_id+'/login');
+                }
+            }
+        });
+    });
+
+    /* GET login page for planiz_id */
+    router.get('/:planiz_id/login', function (req, res) {
+        var o_id = new mongo.ObjectID(req.params.planiz_id);
+        db.collection("planiz").findOne({"_id": o_id}, function (err, result) {
+            if (err) {
+                console.error('Find failed', err);
+            } else {
+                res.render('login', {title:result.title, planiz: result})
+            }
+        });
+    });
+
+    /* GET login page for planiz_id */
+    router.get('/:planiz_id/:user_id/login', function (req, res) {
+        var o_id = new mongo.ObjectID(req.params.planiz_id);
+        db.collection("planiz").findOne({"_id": o_id}, function (err, result) {
+            if (err) {
+                console.error('Find failed', err);
+            } else {
+                var users = result.users;
+                for (var i = 0; i < users.length; i++){
+                    // look for the entry with a matching `user_id` value
+                    if (users[i].id == req.params.user_id){
+                        var user = users[i];
+                        req.session.userId = users[i].id;
+                    }
+                }
+                res.render('dashboard', {title:result.title, planiz: result, user: user})
+            }
+        });
+    });
+
+    /* POST new user for planiz_id */
+    router.post('/:planiz_id/addUser', function (req, res) {
+
+        var o_id = new mongo.ObjectID(req.params.planiz_id);
+        var id_user = uuidv1(); //random id user
+
+        db.collection("planiz").update({"_id": o_id}, { $push: { users: {id: id_user, name: req.body.username} } }, function(err, added) {
+            if( err || !added ) {
+                console.log("User not added.");
+                callback(null,added);
+            }
+            else {
+                console.log("User "+req.body.username+" added to "+o_id);
+                res.redirect('/'+req.params.planiz_id+'/login');
             }
         });
     });
 
     /* GET dates page for planiz_id */
-    router.get('/:planiz_id/dates', function (req, res) {
+    router.get('/:planiz_id/:user_id/dates', function (req, res) {
         var o_id = new mongo.ObjectID(req.params.planiz_id);
         db.collection("planiz").findOne({"_id": o_id}, function (err, result) {
             if (err) {
                 console.error('Find failed', err);
             } else {
-                res.render('dates', {title:result.title, planiz: result})
+                var users = result.users;
+                for (var i = 0; i < users.length; i++){
+                    // look for the entry with a matching `user_id` value
+                    if (users[i].id == req.params.user_id){
+                        var user = users[i];
+                        req.session.userId = users[i].id;
+                    }
+                }
+                res.render('dates', {title:result.title, planiz: result, user:user})
             }
         });
     });
 
     /* GET destinations page for planiz_id */
-    router.get('/:planiz_id/destination', function (req, res) {
+    router.get('/:planiz_id/:user_id/destination', function (req, res) {
         var o_id = new mongo.ObjectID(req.params.planiz_id);
         db.collection("planiz").findOne({"_id": o_id}, function (err, result) {
             if (err) {
                 console.error('Find failed', err);
             } else {
-                res.render('destination', {title:result.title, planiz: result})
+                var users = result.users;
+                for (var i = 0; i < users.length; i++){
+                    // look for the entry with a matching `user_id` value
+                    if (users[i].id == req.params.user_id){
+                        var user = users[i];
+                        req.session.userId = users[i].id;
+                    }
+                }
+                res.render('destination', {title:result.title, planiz: result, user:user})
             }
         });
     });
 
     /* GET budget page for planiz_id */
-    router.get('/:planiz_id/budget', function (req, res) {
+    router.get('/:planiz_id/:user_id/budget', function (req, res) {
         var o_id = new mongo.ObjectID(req.params.planiz_id);
         db.collection("planiz").findOne({"_id": o_id}, function (err, result) {
             if (err) {
                 console.error('Find failed', err);
             } else {
-                res.render('budget', {title:result.title, planiz: result})
+                var users = result.users;
+                for (var i = 0; i < users.length; i++){
+                    // look for the entry with a matching `user_id` value
+                    if (users[i].id == req.params.user_id){
+                        var user = users[i];
+                        req.session.userId = users[i].id;
+                    }
+                }
+                res.render('budget', {title:result.title, planiz: result, user:user})
             }
         });
     });
 
     /* POST new destination option
       * */
-    router.post('/addDestination/:planiz_id', urlEncodedParser, function (req, res) {
+    router.post('/:planiz_id/:user_id/addDestination', urlEncodedParser, function (req, res) {
         console.log("Je suis ici et id = "+req.params.planiz_id)
         var o_id = new mongo.ObjectID(req.params.planiz_id);
         db.collection("planiz").update({"_id": o_id}, { $push: { destinations: req.body.newoption } }, function(err, added) {
@@ -130,14 +223,10 @@ MongoClient.connect("mongodb://localhost/bdd_planiz", function(err, db) {
             }
             else {
                 console.log("Destination"+req.body.newoption+"added to "+o_id);
-                res.redirect('/'+req.params.planiz_id+'/destination');
+                res.redirect('/'+req.params.planiz_id+'/'+req.params.user_id+'/destination');
             }
         });
     });
-
-
-
-
 });
 
 module.exports = router;
