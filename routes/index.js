4078768359c4454ac044410f7aedd05c3ca25d7e
Anca Stoica <ancastoica93@gmail.com>
2017-12-12 20:58:07 +0100

    make redirections for planiz url entered on homepage

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var bodyParser = require('body-parser');
+var urlEncodedParser = bodyParser.urlencoded({extended: false});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,5 +13,11 @@ router.get('/', function(req, res, next) {
 router.get('/createPlaniz', function (req, res) {
     res.render('createPlaniz', { title: 'Create' });
 });
+
+/* POST url where to go to */
+router.post('/goToUrl', urlEncodedParser, function (req, res) {
+    res.redirect(req.body.url);
+});
+
 
 module.exports = router;
