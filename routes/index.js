fe7850ada65962b698c71d0b4e120a0f96d8a91f
Anca Stoica <ancastoica93@gmail.com>
2017-12-12 15:59:12 +0100

    changed header title for index

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Planiz' });
+    res.render('index', { title: 'Homepage' });
 });
 
 /* GET create planiz form page. */
 router.get('/createPlaniz', function (req, res) {
-    res.render('createPlaniz', { title: 'Planiz' });
+    res.render('createPlaniz', { title: 'Create' });
 });
 
 module.exports = router;
