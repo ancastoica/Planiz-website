258c557cd88e2f2e4782710e3958e9ed0005be2d
Anca Stoica <ancastoica93@gmail.com>
2018-01-08 21:35:51 +0100

    enregistrement dans la bdd du choix d'un utilisateur pour les destinations

--- routes/destination.js
+++ routes/destination.js
@@ -35,15 +35,48 @@ MongoClient.connect("mongodb://localhost/bdd_planiz", function(err, db) {
     /* POST new destination option
       * */
     router.post('/:planiz_id/:user_id/addDestination', urlEncodedParser, function (req, res) {
-        console.log("Je suis ici et id = "+req.params.planiz_id)
         var o_id = new mongo.ObjectID(req.params.planiz_id);
         db.collection("planiz").update({"_id": o_id}, { $push: { destinations: req.body.newoption } }, function(err, added) {
+            if( err || !added ) {
+                callback(null,added);
+            }
+            else {
+                res.redirect('/'+req.params.planiz_id+'/'+req.params.user_id+'/destination');
+            }
+        });
+    });
+
+    /* POST destination votes
+    * * */
+    router.post('/:planiz_id/:user_id/addDestinationVote', urlEncodedParser, function (req, res) {
+        console.log("Je suis ici et id = "+req.params.planiz_id)
+        var event = JSON.stringify(req.body);
+        var eventJ = JSON.parse(event);
+        var o_id = new mongo.ObjectID(req.params.planiz_id);
+        db.collection("planiz").update({"_id": o_id, "users.id":req.params.user_id}, { $push: { "users.$.destinations": Object.keys(eventJ)[0]  } }, function(err, added) {
             if( err || !added ) {
                 console.log("Destination not added.");
                 callback(null,added);
             }
             else {
-                console.log("Destination"+req.body.newoption+"added to "+o_id);
+                console.log("Destination added to "+o_id);
+                res.redirect('/'+req.params.planiz_id+'/'+req.params.user_id+'/destination');
+            }
+        });
+    });
+
+
+    /* POST pour enlever une destination si l'user a changé d'avis
+    * * */
+    router.post('/:planiz_id/:user_id/removeDestinationVote', urlEncodedParser, function (req, res) {
+        var event = JSON.stringify(req.body);
+        var eventJ = JSON.parse(event);
+        var o_id = new mongo.ObjectID(req.params.planiz_id);
+        db.collection("planiz").update({"_id": o_id, "users.id":req.params.user_id}, { $pull: { "users.$.destinations": Object.keys(eventJ)[0]  } }, function(err, added) {
+            if( err || !added ) {
+                callback(null,added);
+            }
+            else {
                 res.redirect('/'+req.params.planiz_id+'/'+req.params.user_id+'/destination');
             }
         });
