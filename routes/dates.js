fc1e35d2f2667bf9cf138f5811641fd7ad312632
Anca Stoica <ancastoica93@gmail.com>
2018-01-12 14:37:25 +0100

    fix bug dates

--- routes/dates.js
+++ routes/dates.js
@@ -10,11 +10,7 @@ MongoClient.connect("mongodb://localhost/bdd_planiz", function(err, db) {
     if (err) return funcCallback(err);
 
     /*
-     * GET dates page for planiz_id
-     * Variables renvoyes:
-     * planiz: planiz courant
-     * user: utilisateur courant
-     * events: disponibilites presentes dans la bdd
+     * GET dates page
      */
     router.get('/:planiz_id/:user_id/dates', function (req, res) {
         var o_id = new mongo.ObjectID(req.params.planiz_id);
@@ -81,6 +77,9 @@ MongoClient.connect("mongodb://localhost/bdd_planiz", function(err, db) {
         });
     });
 
+    /*
+     * Calcul des meilleures plages de dates
+     */
     router.get('/:planiz_id/:user_id/computeBestAvailabilities', function (req, res) {
         var o_id = new mongo.ObjectID(req.params.planiz_id);
         db.collection("planiz").findOne({"_id": o_id}, function (err, r) {
@@ -92,6 +91,7 @@ MongoClient.connect("mongodb://localhost/bdd_planiz", function(err, db) {
             var best = r.bestAvailabilities;
 
             for(var d=1; d<=r.users.length;d++){
+                //console.log("d: "+d);
 
                 var key = keys[0];
                 var previousKey = key;
@@ -100,6 +100,8 @@ MongoClient.connect("mongodb://localhost/bdd_planiz", function(err, db) {
                 var start1 = new Date(start0+"Z");
                 var end1 = new Date(end0+"Z");
                 var diff1 = (end1-start1)/(1000*60*60*24);
+
+                //console.log("start: "+ start0);
 
                 if(r.bestAvailabilities[""+d]){
                     var start2 = new Date(r.bestAvailabilities[""+d].start+"Z");
@@ -112,27 +114,35 @@ MongoClient.connect("mongodb://localhost/bdd_planiz", function(err, db) {
                 for (var k = 1; k < keys.length; k++) {
                     key = keys[k];
 
+
                     var d1 = new Date(key+"Z");
                     var d2 = new Date(previousKey+"Z");
                     var diff = (d1-d2)/(1000*60*60*24);
 
                     if (d <= a[key] && diff === 1  ) {
+                        if(d > a[previousKey]){
+                            start0 = key;
+                            start1 = new Date(start0+"Z");
+                        }
                         end0 = key;
+                        //console.log("increased end: "+ end0);
                         end1 = new Date(end0+"Z");
+
+                        diff1 = (end1-start1)/(1000*60*60*24);
+
+                        if(r.bestAvailabilities[""+d] && diff1 > diff2 || !r.bestAvailabilities[""+d]){
+                            best[d] = {
+                                start: start0,
+                                end: end0
+                            };
+                            db.collection("planiz").update({"_id": o_id}, {$set: {"bestAvailabilities":best}}, function (err, added) {});
+                        }
                     } else {
                         start0 = key;
                         end0 = key;
                         start1 = new Date(start0+"Z");
                         end1 = new Date(end0+"Z");
-                    }
-                    diff1 = (end1-start1)/(1000*60*60*24);
-
-                    if(r.bestAvailabilities[""+d] && diff1 > diff2 || !r.bestAvailabilities[""+d]){
-                        best[d] = {
-                            start: start0,
-                            end: end0
-                        };
-                        db.collection("planiz").update({"_id": o_id}, {$set: {"bestAvailabilities":best}}, function (err, added) {});
+                        //console.log("new period start, end: "+ end0);
                     }
                     previousKey = key;
                 }
